4a74798467466a049fb4f7beed46e357e35edb76
Mark <mdarens@gmail.com>
2019-05-28 16:11:16 -0500

    Bump excerpt count on home

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,26 +6,31 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <StaticQuery query={graphql`
+    <StaticQuery
+      query={graphql`
         query WordPressHomeQuery {
-        allWordpressPost(limit: 2) {
-          edges {
-            node {
-              title
-              slug
-              excerpt
+          allWordpressPost(limit: 10) {
+            edges {
+              node {
+                title
+                slug
+                excerpt
+              }
             }
           }
         }
+      `}
+      render={data =>
+        data.allWordpressPost.edges.map(({ node }) => (
+          <article>
+            <Link to={`/posts/${node.slug}`}>
+              <h2 dangerouslySetInnerHTML={{ __html: node.title }} />
+            </Link>
+            <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+          </article>
+        ))
       }
-    `} render={data => data.allWordpressPost.edges.map(({ node }) => (
-      <article>
-        <Link to={`/posts/${node.slug}`}>
-          <h2 dangerouslySetInnerHTML={{ __html: node.title }} />
-        </Link>
-        <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-      </article>
-    ))} />
+    />
     <Link to="/events/">Events</Link>
   </Layout>
 )

fcf00cd957d2dc487976adc1f3582630713f0389
Mark D'Arensbourg <mdarens@gmail.com>
2019-06-01 16:52:24 -0500

    margin once more

--- src/components/layout.js
+++ src/components/layout.js
@@ -46,10 +46,10 @@ const Layout = ({ children }) => (
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
-            margin: `80px auto 0 auto`,
+            margin: `0 auto`,
             maxWidth: 960,
             padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
+            paddingTop: 80,
           }}
         >
           <main>{children}</main>

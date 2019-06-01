b6ebfbed5e7203d0b7735da7ab43a52e37ebe467
Mark D'Arensbourg <mdarens@gmail.com>
2019-06-01 16:48:25 -0500

    margin for nav

--- src/components/layout.js
+++ src/components/layout.js
@@ -46,7 +46,7 @@ const Layout = ({ children }) => (
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
-            margin: `0 auto`,
+            margin: `80px auto 0 auto`,
             maxWidth: 960,
             padding: `0px 1.0875rem 1.45rem`,
             paddingTop: 0,

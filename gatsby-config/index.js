05bf4ddb9c21fd328da53aa5a4311719e6a1f3d6
Mark <mdarens@gmail.com>
2019-06-05 10:09:24 -0500

    Cache bust the netlify way

--- gatsby-config/index.js
+++ gatsby-config/index.js
@@ -16,6 +16,7 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-jss`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify-cache`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {

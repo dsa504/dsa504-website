8d0bf7dcf747553b6f938a0490b18e25a969a054
unknown <mdarens@gmail.com>
2019-06-15 10:10:24 -0500

    Add more page titles

--- src/components/committee.jsx
+++ src/components/committee.jsx
@@ -1,5 +1,11 @@
 import React from "react"
+import SEO from "./seo"
 
-const Committee = ({ pageContext }) => <h1>{pageContext.title}</h1>
+const Committee = ({ pageContext }) => (
+  <>
+    <SEO title={pageContext.title} />
+    <h1>{pageContext.title}</h1>
+  </>
+)
 
 export default Committee
--- src/components/event-detail/index.jsx
+++ src/components/event-detail/index.jsx
@@ -1,6 +1,7 @@
 /* eslint-env node */
 import React from "react"
 import { graphql } from "gatsby"
+import SEO from "../seo"
 
 const EventDetail = ({
   data: {
@@ -8,30 +9,33 @@ const EventDetail = ({
   },
 }) => {
   return (
-    <article>
-      <div style={{ display: "flex" }}>
-        <div
-          style={{
-            maxWidth: 400,
-            height: 400,
-            flex: "1 0 auto",
-            backgroundImage: mapImage
-              ? `url(${mapImage.childImageSharp.fluid.base64})`
-              : null,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-          }}
-        >
-          {mapImage ? (
-            <img style={{ maxWidth: "100%" }} src={mapImage.publicURL} />
-          ) : null}
+    <>
+      <SEO title={summary} />
+      <article>
+        <div style={{ display: "flex" }}>
+          <div
+            style={{
+              maxWidth: 400,
+              height: 400,
+              flex: "1 0 auto",
+              backgroundImage: mapImage
+                ? `url(${mapImage.childImageSharp.fluid.base64})`
+                : null,
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+          >
+            {mapImage ? (
+              <img style={{ maxWidth: "100%" }} src={mapImage.publicURL} />
+            ) : null}
+          </div>
+          <div>
+            <h1>{summary}</h1>
+            <div dangerouslySetInnerHTML={{ __html: description }} />
+          </div>
         </div>
-        <div>
-          <h1>{summary}</h1>
-          <div dangerouslySetInnerHTML={{ __html: description }} />
-        </div>
-      </div>
-    </article>
+      </article>
+    </>
   )
 }
 

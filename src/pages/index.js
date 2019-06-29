be54a5c81fe3ae5d541a244c171ad498f0bddd09
unknown <mdarens@gmail.com>
2019-06-29 10:07:45 -0500

    Add the most rudimentary responsive layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import SEO from "../components/seo"
 import HomeCalendar from "../components/home-calendar"
 import HomePost from "../components/home-post"
 import HomeCommittees from "../components/home-committees"
+import useSheet from "react-jss"
 
 const IndexPage = () => (
   <>
@@ -47,33 +48,68 @@ const IndexPage = () => (
           }
         }
       `}
-      render={HomeRoot}
+      // Expects a function component not a class which the react-jss HOC makes it,
+      // and the hook-based API doesn't seem to work here yet so we're stuck with this
+      render={HomeRootWrapper}
     />
   </>
 )
 
-const HomeRoot = ({
+const _HomeRoot = ({
   allWordpressPost,
   allWordpressWpCommittee,
   allCalendarEvent,
+  classes,
 }) => {
   return (
-    <div style={{ display: "flex" }}>
-      <div>
+    <div>
+      <div className={classes.upcoming}>
+        <div>
+          <HomeCommittees
+            committees={allWordpressWpCommittee.edges.map(edge => edge.node)}
+          />
+          <br />
+          <br />
+        </div>
+        <div>
+          <HomeCalendar
+            events={allCalendarEvent.edges.map(edge => edge.node)}
+          />
+        </div>
+      </div>
+      <div className={classes.posts}>
         {allWordpressPost.edges.map(({ node }) => (
           <HomePost key={node.slug} {...node} />
         ))}
       </div>
-      <div style={{ flex: "1 0 auto" }}>
-        <HomeCommittees
-          committees={allWordpressWpCommittee.edges.map(edge => edge.node)}
-        />
-        <br />
-        <br />
-        <HomeCalendar events={allCalendarEvent.edges.map(edge => edge.node)} />
-      </div>
     </div>
   )
 }
+
+const styles = {
+  column: {
+    padding: ["1em", "2em"],
+  },
+  posts: {
+    composes: "$column",
+  },
+  upcoming: {
+    composes: "$column",
+    "@media screen and (min-width: 45em)": {
+      columns: 2,
+      "& > div": {
+        breakInside: "avoid",
+      },
+    },
+    "@media screen and (min-width: 65em)": {
+      float: "right",
+      columns: 1,
+    },
+  },
+}
+
+const HomeRoot = useSheet(styles)(_HomeRoot)
+
+const HomeRootWrapper = props => <HomeRoot {...props} />
 
 export default IndexPage

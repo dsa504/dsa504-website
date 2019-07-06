df6b83a5ccc943c26dc7a852e0c61cabd58827cb
unknown <mdarens@gmail.com>
2019-07-06 13:30:35 -0500

    Don't inject wp-blocks library on home layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -12,7 +12,6 @@ import Header from "../components/header"
 import Image from "../components/image"
 import "./layout.css"
 import "../sass/style.scss"
-import "../sass/wp-block-library.scss"
 
 const Layout = ({ pageContext, children }) => (
   <StaticQuery
@@ -25,66 +24,72 @@ const Layout = ({ pageContext, children }) => (
         }
       }
     `}
-    render={data => (
-      <>
-        <div id="container">
-          <Header siteTitle={data.site.siteMetadata.title} />
-          {pageContext.layout === "home" ? (
-            <div className="hero-index">
-              <div className="wrap hero-wrap">
-                <Image
-                  imgName="dsanola_FBpage_banner-01.png"
-                  className="hero-index-img"
-                  alt={`A stylized map of the New Orleans riverfront captioned with "A better world is possible"`}
-                  title={`A stylized map of the New Orleans riverfront captioned with " A better world is possible"`}
-                />
-              </div>
-            </div>
-          ) : null}
-
-          <main id="content" className="wrap">
-            {children}
-          </main>
-          <footer>
-            <div className="footer" style={{ padding: "1em 2em" }}>
-              <nav className="pull-left">
-                <a itemProp="url" href="/">
+    render={data => {
+      const isHome = pageContext.layout === "home"
+      if (!isHome) {
+        import("../sass/wp-block-library.scss")
+      }
+      return (
+        <>
+          <div id="container">
+            <Header siteTitle={data.site.siteMetadata.title} />
+            {isHome ? (
+              <div className="hero-index">
+                <div className="wrap hero-wrap">
                   <Image
-                    style={{ width: 114, height: 100 }}
-                    imgName="dsa-new-orleans-logo-footer.png"
-                    alt="DSA New Orleans"
-                    title="DSA New Orleans"
+                    imgName="dsanola_FBpage_banner-01.png"
+                    className="hero-index-img"
+                    alt={`A stylized map of the New Orleans riverfront captioned with "A better world is possible"`}
+                    title={`A stylized map of the New Orleans riverfront captioned with " A better world is possible"`}
                   />
-                </a>
-                <span
-                  className="microdata-logo"
-                  itemProp="logo"
-                  style={{ display: "none" }}
-                >
-                  <Image imgName="equal-access-legal-logo.png" />
-                </span>
-              </nav>
-              <div className="copyright">
-                <p>
-                  © {new Date().getFullYear()}{" "}
-                  <span itemProp="name">DSA New Orleans</span>
-                  <br className="visible-xs" />
-                  <span style={{ fontSize: "10px", color: "#7b7b7b" }}>
-                    All rights reserved.
+                </div>
+              </div>
+            ) : null}
+
+            <main id="content" className="wrap">
+              {children}
+            </main>
+            <footer>
+              <div className="footer" style={{ padding: "1em 2em" }}>
+                <nav className="pull-left">
+                  <a itemProp="url" href="/">
+                    <Image
+                      style={{ width: 114, height: 100 }}
+                      imgName="dsa-new-orleans-logo-footer.png"
+                      alt="DSA New Orleans"
+                      title="DSA New Orleans"
+                    />
+                  </a>
+                  <span
+                    className="microdata-logo"
+                    itemProp="logo"
+                    style={{ display: "none" }}
+                  >
+                    <Image imgName="equal-access-legal-logo.png" />
                   </span>
-                </p>
+                </nav>
+                <div className="copyright">
+                  <p>
+                    © {new Date().getFullYear()}{" "}
+                    <span itemProp="name">DSA New Orleans</span>
+                    <br className="visible-xs" />
+                    <span style={{ fontSize: "10px", color: "#7b7b7b" }}>
+                      All rights reserved.
+                    </span>
+                  </p>
+                </div>
+                <div className="additional-links">
+                  Get in Touch!
+                  <br />{" "}
+                  <a href="hello@dsaneworleans.org">hello@dsaneworleans.org</a>
+                </div>
               </div>
-              <div className="additional-links">
-                Get in Touch!
-                <br />{" "}
-                <a href="hello@dsaneworleans.org">hello@dsaneworleans.org</a>
-              </div>
-            </div>
-            <br className="clearit" />
-          </footer>
-        </div>
-      </>
-    )}
+              <br className="clearit" />
+            </footer>
+          </div>
+        </>
+      )
+    }}
   />
 )
 
